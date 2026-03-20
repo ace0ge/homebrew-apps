15e97496c2f247619708dfa4bf61cb2c6959c1cd
Ace <geguobin@gmail.com>
2026-03-21 02:44:49 +0800

    Simplify cask - minimal version

--- Casks/clashmac.rb
+++ Casks/clashmac.rb
@@ -3,28 +3,11 @@ cask "clashmac" do
 
   version "26.7-beta.4"
 
-  url "https://github.com/666OS/ClashMac/releases/download/#{version}/ClashMac-#{version}-macos-#{arch}.zip",
-      verified: "github.com/666OS/ClashMac/"
+  url "https://github.com/666OS/ClashMac/releases/download/#{version}/ClashMac-#{version}-macos-#{arch}.zip"
 
   name "ClashMac"
   desc "Native Proxy Experience Built for macOS"
   homepage "https://clashmac.app/"
 
-  livecheck do
-    strategy :github_latest
-    regex(/^(\d+\.\d+(?:-\w+(?:\.\d+)?)?)$/)
-  end
-
-  auto_updates true
-  depends_on macos: ">= :monterey"
-
   app "ClashMac-#{version}-macos-#{arch}/ClashMac.app"
-
-  zap trash: [
-    "~/Library/Application Support/clashmac",
-    "~/Library/Caches/com.clashmac.app",
-    "~/Library/HTTPStorages/com.clashmac.app",
-    "~/Library/Preferences/com.clashmac.app.plist",
-    "~/Library/Saved Application State/com.clashmac.app.savedState",
-  ]
 end
